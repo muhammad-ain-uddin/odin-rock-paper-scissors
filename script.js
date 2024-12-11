64df2615632dd8257a5463b3139b431765445e2a
Muhammad Ain Uddin <hmuhammadain@gmail.com>
2024-12-11 12:59:16 +0500

    Implement playRound function and update user input handling
    - Added playRound() to determine round winner and implemented
    game logic for comparing player and computer choices
    - Modified getHumanChoice() to handle edge cases

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -12,8 +15,15 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt("Rock, Paper or Scissors?");
+
+  // User cancelled the prompt; exit the function
+  if (humanChoice === null) {
+    return;
+  }
+
   humanChoice = humanChoice.toLowerCase(); // Convert the input to lowercase
 
+  // Check if the input is a valid choice
   if (
     humanChoice === "rock" ||
     humanChoice === "paper" ||
@@ -22,8 +32,38 @@ function getHumanChoice() {
     return humanChoice; // return the valid input
   } else {
     console.log("Enter a valid choice: rock, paper or scissors.");
-    return getHumanChoice(); // Recursiively ask for input until valid
+    return getHumanChoice(); // Recursively ask for input until valid
   }
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    return 0;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win!");
+    return 1;
+  } else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    console.log("Computer wins!");
+    return 2;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+if (humanSelection === undefined) {
+  console.log(
+    "Looks like you cancelled the game. To start over, just refresh the page and you'll be good to go!"
+  );
+} else {
+  playRound(humanSelection, computerSelection);
+}
