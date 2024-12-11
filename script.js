6703d1deeedc9f0cc3df6d7d1ca1ef7f47fa2287
Muhammad Ain Uddin <hmuhammadain@gmail.com>
2024-12-11 11:54:48 +0500

    Implement player and computer choice selection functions
    - Created getComputerChoice() to randomly generate computer's move
    - Implemented getHumanChoice() with input validation and recursive prompt

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("hello world!");
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+  if (computerChoice === 1) {
+    return "rock";
+  } else if (computerChoice === 2) {
+    return "paper";
+  } else if (computerChoice === 3) {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("Rock, Paper or Scissors?");
+  humanChoice = humanChoice.toLowerCase(); // Convert the input to lowercase
+
+  if (
+    humanChoice === "rock" ||
+    humanChoice === "paper" ||
+    humanChoice === "scissors"
+  ) {
+    return humanChoice; // return the valid input
+  } else {
+    console.log("Enter a valid choice: rock, paper or scissors.");
+    return getHumanChoice(); // Recursiively ask for input until valid
+  }
+}
+
+console.log(getHumanChoice());
